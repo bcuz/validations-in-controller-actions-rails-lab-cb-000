014179ebfec2da1afc466bedd5d6bf861772c56b
adam n <adam.nawroc@gmail.com>
2018-04-10 17:14:56 +0000

    progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,22 @@ class PostsController < ApplicationController
 
   def update
     # binding.pry
-    @post.update(post_params)
+    # 
+    # redirect_to post_path(@post)
 
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+
+      # If--and only if--the post is valid, do what we usually do.
+      # @post.save
+      # This returns a status_code of 302, which instructs the browser to
+      # worry about re-reading it from the database)
+      redirect_to post_path(@post)
+    else
+      # If the post is invalid, hold on to @post, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+      render :edit
+    end
   end
 
   private
